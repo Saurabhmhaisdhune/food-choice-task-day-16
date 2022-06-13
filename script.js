2865dfb2636a54ea365a9091759910ba9b6ecd6b
Saurabh Lahanu Mhaisdhune <103380774+Saurabhmhaisdhune@users.noreply.github.com>
2022-06-14 00:43:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function myfunction() {
+    var txt1 = document.getElementById("firstname").value;
+    document.getElementById("demo").innerHTML = txt1;
+    var txt2 = document.getElementById("lastname").value;
+    document.getElementById("demo1").innerHTML = txt2;
+    var txt3 = document.getElementById("address").value;
+    document.getElementById("demo2").innerHTML = txt3;
+    var txt4 = document.getElementById("pincode").value;
+    document.getElementById("demo3").innerHTML = txt4;
+    var txt5 = document.getElementById("country").value;
+    document.getElementById("demo4").innerHTML = txt5;
+    var txt6 = document.getElementById("state").value;
+    document.getElementById("demo5").innerHTML = txt6;
+    var txt7 = document.getElementById("gender").value;
+    document.getElementById("demo6").innerHTML =txt7;
+
+
+    var txt8 = document.getElementById("menu1").option.value;
+    document.getElementById("demo7").innerHTML =txt8;
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
